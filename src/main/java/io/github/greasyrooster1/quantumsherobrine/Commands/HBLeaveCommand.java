ef6ae0d83453c5e57644bcac2cf1748f6c3b8f77
QuantumDeity <greasyRooster1@gmail.com>
2023-12-18 17:20:53 -0800

    added hbleave command

---
+++ src/main/java/io/github/greasyrooster1/quantumsherobrine/Commands/HBLeaveCommand.java
@@ -0,0 +1,38 @@
+package io.github.greasyrooster1.quantumsherobrine.Commands;
+
+import io.github.greasyrooster1.quantumsherobrine.CommandBase;
+import io.github.greasyrooster1.quantumsherobrine.Util.Msg;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HBLeaveCommand {
+    public void register(){
+        new CommandBase("hbleave",false){
+            @Override
+            public boolean onCommand(@NotNull CommandSender sender, @NotNull String[] args) {
+                Msg.broadcast(sender,"&eHerobrine left the game");
+                return true;
+            }
+
+            @Override
+            public String getUsage() {
+                return "/hbleave";
+            }
+
+            @Override
+            public @NotNull List<String> getAliases() {
+                List<String> aliases = new ArrayList<>();
+                aliases.add("hleave");
+                return aliases;
+            }
+
+            @Override
+            public @NotNull String getDescription() {
+                return "[QH] send herobrine leave message";
+            }
+        }.setOp(true);
+    }
+}
