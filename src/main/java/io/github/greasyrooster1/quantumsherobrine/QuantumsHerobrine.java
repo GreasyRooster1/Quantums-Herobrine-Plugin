76d7ca40f9c762a2e920f360e1b9060ef26bd840
QuantumDeity <greasyRooster1@gmail.com>
2023-12-19 14:57:03 -0800

    fixed ghost herobrine

--- src/main/java/io/github/greasyrooster1/quantumsherobrine/QuantumsHerobrine.java
+++ src/main/java/io/github/greasyrooster1/quantumsherobrine/QuantumsHerobrine.java
@@ -1,8 +1,12 @@
 package io.github.greasyrooster1.quantumsherobrine;
 
 import io.github.greasyrooster1.quantumsherobrine.Commands.*;
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Iterator;
 
 public final class QuantumsHerobrine extends JavaPlugin {
     static QuantumsHerobrine plugin;
@@ -22,6 +26,14 @@ public final class QuantumsHerobrine extends JavaPlugin {
         new FeedCommand().register();
         new ExplodeCommand().register();
         registerHerobrineCommands();
+        clearCitizens();
+    }
+
+    public void clearCitizens(){
+        for (Iterator<NPC> it = CitizensAPI.getNPCRegistry().iterator(); it.hasNext(); ) {
+            NPC npc = it.next();
+            npc.despawn();
+        }
     }
 
     public void registerHerobrineCommands(){
